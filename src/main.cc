726e20d2f2125d5d93ec652bbc2d09403f17caf1
Martin Benes <martinbenesh@gmail.com>
2013-10-13 21:02:50 +0200

    minor changes

--- src/main.cc
+++ src/main.cc
@@ -17,13 +17,13 @@ int main(int argc, char* argv[]){
 	YafImport import;
 	if(argc != 2){
 		cout<<"Usage: ray [input_file]"<<endl;
-		return 0;
+		return 1;
 	}
 	auto_ptr<Scene> scene( import.import(argv[1]) );
 
 	if(scene.get() == 0){
 		cerr<<"error loading scene"<<endl;
-		return 0;
+		return 1;
 	}
 	string outputFile(argv[1]);
 
